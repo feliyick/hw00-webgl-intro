c4f2c9dc67369d7d68d164abd3374c4b25b5f39f
Felicity Yick <feliyick@seas.upenn.edu>
2021-09-11 15:54:40 -0400

    finished ver 1

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
-import {vec3} from 'gl-matrix';
+import {vec3, vec4} from 'gl-matrix';
 const Stats = require('stats-js');
 import * as DAT from 'dat.gui';
 import Icosphere from './geometry/Icosphere';
 import Square from './geometry/Square';
+import Cube from './geometry/Cube'
 import OpenGLRenderer from './rendering/gl/OpenGLRenderer';
 import Camera from './Camera';
 import {setGL} from './globals';
@@ -13,17 +14,22 @@ import ShaderProgram, {Shader} from './rendering/gl/ShaderProgram';
 const controls = {
   tesselations: 5,
   'Load Scene': loadScene, // A function pointer, essentially
+  palette: [255, 255, 255]
 };
 
 let icosphere: Icosphere;
 let square: Square;
+let cube: Cube;
 let prevTesselations: number = 5;
+let time: number = 0;
 
 function loadScene() {
   icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, controls.tesselations);
   icosphere.create();
   square = new Square(vec3.fromValues(0, 0, 0));
   square.create();
+  cube =   new Cube(vec3. fromValues(0, 0, 0));
+  cube.create();
 }
 
 function main() {
@@ -39,6 +45,8 @@ function main() {
   const gui = new DAT.GUI();
   gui.add(controls, 'tesselations', 0, 8).step(1);
   gui.add(controls, 'Load Scene');
+  let colourController = gui.addColor(controls, 'palette')
+  
 
   // get canvas and webgl context
   const canvas = <HTMLCanvasElement> document.getElementById('canvas');
@@ -64,6 +72,12 @@ function main() {
     new Shader(gl.FRAGMENT_SHADER, require('./shaders/lambert-frag.glsl')),
   ]);
 
+  const customNoise = new ShaderProgram([
+    new Shader(gl.VERTEX_SHADER, require('./shaders/customNoise-vert.glsl')),
+    new Shader(gl.FRAGMENT_SHADER, require('./shaders/customNoise-frag.glsl')),
+  ]);
+
+
   // This function will be called every frame
   function tick() {
     camera.update();
@@ -76,10 +90,37 @@ function main() {
       icosphere = new Icosphere(vec3.fromValues(0, 0, 0), 1, prevTesselations);
       icosphere.create();
     }
-    renderer.render(camera, lambert, [
+
+    //lambert render
+    // colourController.onChange( function( colour ) {
+    //   let col = vec4.fromValues(colour[0] / 255.0, 
+    //                             colour[1] / 255.0, 
+    //                             colour[2] / 255.0, 1)
+    //   lambert.setGeometryColor(col)
+    // })
+    // lambert.setTime(time);
+    // time++;
+    // renderer.render(camera, lambert, [
+    //   // icosphere,
+    //   // square,
+    //   cube
+    // ]);
+    
+    //custom noise render
+    colourController.onChange( function( colour ) {
+      let col = vec4.fromValues(colour[0] / 255.0, 
+                                colour[1] / 255.0, 
+                                colour[2] / 255.0, 1)
+      customNoise.setGeometryColor(col)
+    })
+    customNoise.setTime(time);
+    time++;
+    renderer.render(camera, customNoise, [
       icosphere,
       // square,
+      // cube
     ]);
+
     stats.end();
 
     // Tell the browser to call `tick` again whenever it renders a new frame
